c8973332a698187d4f7b4e069d38efbbf3ef6170
mogoatlhe <iam@ofent.se>
2023-07-06 20:35:36 +0200

    create getnewnode method

--- src/models/LinkedList.js
+++ src/models/LinkedList.js
@@ -1,23 +1,30 @@
-import Node from "./Node";
+import Node from "./Node.js";
 
 const LinkedList = () => {
   let head = null;
 
   const appendValue = (value) => {
     if (head === null) {
-      head = Node();
-      head.setValue(value);
+      head = _getNewNode(value);
       return;
     }
 
     let curr = head;
 
     while (curr !== null && curr.next !== null) {
+      console.log(curr.getValue());
       curr = curr.next;
     }
 
-    curr.next = Node();
-    curr.setValue(value);
+    console.log();
+    curr.next = _getNewNode(value);
+  };
+
+  const _getNewNode = (value) => {
+    const node = Node();
+    node.setValue(value);
+
+    return node;
   };
 
   return {
